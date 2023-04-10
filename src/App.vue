11af30dddd0e94adaa6c4af709a15a47af2fbe49
Sidney François <sidney.fvm.dev@outlook.com>
2023-04-10 18:04:16 -0300

    add style into App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,23 @@
 <script setup lang="ts">
-import InformationComponent from './components/InformationComponent.vue';
+import InformationComponent from "./components/InformationComponent.vue";
 </script>
 
 <template>
-
   <InformationComponent />
   <!-- <EventTest />
   <HelloWorld msg="Vite + Vue" /> -->
 </template>
-
-
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
