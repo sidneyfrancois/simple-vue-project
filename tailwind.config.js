39a1eb651d8e1968d27d9097e8e4b1b9be535a65
Sidney François <sidney.fvm.dev@outlook.com>
2023-04-10 19:55:57 -0300

    remove styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  darkMode: 'media',
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-  ],
+  darkMode: "media",
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
