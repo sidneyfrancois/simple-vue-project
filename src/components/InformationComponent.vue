2ddb69fda73698bd2c662201b92cbce68c39815e
Sidney François <sidney.fvm.dev@outlook.com>
2023-04-10 18:39:06 -0300

    add computed property

--- src/components/InformationComponent.vue
+++ src/components/InformationComponent.vue
@@ -16,6 +16,17 @@ export default {
     addNewItem() {
       this.obj.arr.push("test");
     },
+    removeItem() {
+      this.obj.arr.pop();
+    },
+  },
+  computed: {
+    // computed properties are cached based on their reactive dependencies
+    // computed properties are getter-only, must provide manually a getter and setter
+    // must be free of side effects
+    hasElements() {
+      return this.obj.arr.length > 0 ? "Yes" : "No";
+    },
   },
   mounted() {
     console.log(this.count);
@@ -28,6 +39,11 @@ export default {
   Cout is {{ count }}
 
   <button @click="addNewItem">Add new element</button>
+  <button @click="removeItem">Remove element</button>
   <h2>{{ obj.arr[obj.arr.length - 1] }}</h2>
   <h3>{{ obj.arr }}</h3>
+
+  <br />
+
+  <h2>Has elements: {{ hasElements }}</h2>
 </template>
