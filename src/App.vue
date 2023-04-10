f60d12491c1a6b83a8a29732bb76f7235ec576cb
Sidney François <sidney.fvm.dev@outlook.com>
2023-04-10 17:43:55 -0300

    remove older components

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,8 @@
 <script setup lang="ts">
-import EventTest from './components/EventTest.vue';
-import HelloWorld from './components/HelloWorld.vue'
-import Card from './components/Card.vue';
+
 </script>
 
 <template>
-  <Card p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4/>
   <h1 class="text-3xl font-bold underline">
     Hello world!
   </h1>
