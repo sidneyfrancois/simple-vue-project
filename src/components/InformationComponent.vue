c6207c6d6c1357ab2cd93d79ed541b50eb0c52cf
Sidney François <sidney.fvm.dev@outlook.com>
2023-04-10 18:08:17 -0300

    test nested mutation

--- src/components/InformationComponent.vue
+++ src/components/InformationComponent.vue
@@ -3,11 +3,18 @@ export default {
   data() {
     return {
       count: 1,
+      obj: {
+        nested: { count: 0 },
+        arr: ["foo", "bar"],
+      },
     };
   },
   methods: {
     increment() {
       this.count++;
+    },
+    addNewItem() {
+      this.obj.arr.push("test");
     },
   },
   mounted() {
@@ -19,4 +26,8 @@ export default {
 <template>
   <button @click="increment">Click to Add</button>
   Cout is {{ count }}
+
+  <button @click="addNewItem">Add new element</button>
+  <h2>{{ obj.arr[obj.arr.length - 1] }}</h2>
+  <h3>{{ obj.arr }}</h3>
 </template>
