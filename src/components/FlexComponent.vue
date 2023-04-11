4473010f48b161114b3feb27804c1fa306eddb04
Sidney François <sidney.fvm.dev@outlook.com>
2023-04-11 15:54:10 -0300

    add button to change background

--- src/components/FlexComponent.vue
+++ src/components/FlexComponent.vue
@@ -1,17 +1,31 @@
 <script lang="ts">
 export default {
   data() {
-    return {};
+    return {
+      isPink: true,
+    };
   },
-  methods: {},
+  methods: {
+    toggleBackgroundPink() {
+      this.isPink = !this.isPink;
+    },
+  },
 };
 </script>
 <template>
   <div class="flex-container">
-    <div class="flex-item">Flex Item 1</div>
+    <div class="flex-item">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, aut vel,
+      deserunt nulla recusandae, consequatur voluptas blanditiis magni aliquam
+      unde laudantium. Sed aperiam quibusdam explicabo, cumque eius sunt! Nihil
+      consectetur distinctio explicabo obcaecati necessitatibus placeat, sint
+      quas facere sit dicta laborum quaerat neque quia esse labore vitae velit,
+      corrupti incidunt.
+    </div>
     <div class="flex-item">Flex Item 2</div>
-    <div class="flex-item">Flex Item 3</div>
+    <div class="flex-item" :class="{ pinkBackground: isPink }">Flex Item 3</div>
   </div>
+  <button @click="toggleBackgroundPink">Toggle Background Pink</button>
 </template>
 <style>
 .flex-container {
@@ -19,13 +33,19 @@ export default {
   max-width: 60rem;
   border: 5px solid #000;
   padding: 2em;
+  display: flex;
 }
 
 .flex-item {
-  margin: 0 5rem;
+  margin: 0.5rem;
   border: 5px solid #aaa;
   background-color: #000;
   color: #fff;
   padding: 0.5em;
+  width: 100%;
+}
+
+.pinkBackground {
+  background-color: pink;
 }
 </style>
